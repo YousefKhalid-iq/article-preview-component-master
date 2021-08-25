776b988986f2621f024aeaded8bcd941ce83e34d
Yousef Khalid <84613950+YousefKhalid-iq@users.noreply.github.com>
2021-08-25 21:53:21 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const share = document.querySelector(".share-img-bg");
 const active = document.querySelector(".share-change");
 
 share.addEventListener("click", function () {
-	active.classList.toggle("show");
+	active.classList.toggle("press");
 });
